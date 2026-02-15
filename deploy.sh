182d98d7c4756ffbc510ca838285fe21dc7f5700
Toni Gurgui <toni.gurgui@gmail.com>
2026-02-15 18:07:19 +0000

    Add script to deploy and configure

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 1. Update repository
+git pull origin main
+
+# 2. Update all Docker stacks
+for d in stacks/*/ ; do
+    echo "Updating $d..."
+    docker-compose -f "${d}docker-compose.yml" up -d --remove-orphans
+done
+
+# 3. Sync DNS records
+sudo python3 ./mdns-sync.py
