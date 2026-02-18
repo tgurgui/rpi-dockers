c74c0454b946d90b190ce6554353b9a91eb799af
Toni Gurguí <toni.gurgui@gmail.com>
2026-02-18 21:44:22 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,29 @@
 # 1. Update repository
 git pull origin master
 
-# 2. Update all Docker stacks
-for d in stacks/*/ ; do
-    echo "Updating $d..."
-    docker-compose -f "${d}docker-compose.yml" up -d --remove-orphans
+# 2. Create network
+NETWORK_NAME="proxy-nw"
+if ! docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+    docker network create $NETWORK_NAME
+fi
+
+# 3. Update all Docker stacks
+# Get the absolute path of the directory where this script is located
+BASE_DIR=$(pwd)
+STACKS_DIR="$BASE_DIR/stacks"
+
+for stack in "$STACKS_DIR"/*/; do
+    stack_name=$(basename "$stack")
+    if [ -f "$stack/docker-compose.yml" ] || [ -f "$stack/docker-compose.yaml" ]; then
+        echo "----------------------------------------------------"
+        echo "Deploying Stack: $stack_name"
+        cd "$stack" || continue
+        
+        docker compose up -d --remove-orphans
+
+        cd "$BASE_DIR"
+    fi
 done
 
-# 3. Sync DNS records
+# 4. Sync DNS records
 sudo python3 ./mdns-sync.py
